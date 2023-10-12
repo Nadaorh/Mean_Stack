99c15979e662327bd64f9c732a2d05f22462e8c9
Nada <rahali.nada03@gmail.com>
2023-10-12 15:12:54 +0300

    Mean_Stack project

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://rahalinada03:z8MkHNao49XA0vP6@ds12226.mlab.com:12226/meanauthapp',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
